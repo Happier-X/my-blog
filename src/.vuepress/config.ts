1e58f83d0be71be49066dc316cab63fc05c8acf5
zhf521 <3407085928@qq.com>
2024-05-13 21:13:23 +0800

    更新基础路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/MyBlog/",
+  base: "/",
 
   lang: "zh-CN",
   title: "茕茕の博客",

7f4d3d1b0f48314961035064859edbfe4f474878
茕茕 <61277974+zhf521@users.noreply.github.com>
2024-07-08 14:49:31 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,9 +4,10 @@ export default sidebar({
   "/SoftwareTool/": "structure",
   "/STM32LearningRecord/": "structure",
   "/DevelopmentSkills/": "structure",
-  "/NestJSLearningRecord/": "structure",
+  "/NestLearningRecord/": "structure",
   "/PhotographyLearningRecord/": "structure",
   "/TravelRecord": "structure",
   "/3x3x3": "structure",
   "/3x3x3BLD": "structure",
+  "/GoLearningRecord/": "structure",
 });

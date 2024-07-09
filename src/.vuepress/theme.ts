8c764794c9c71dceb36922320639639066709fd7
zhf521 <3407085928@qq.com>
2024-07-09 21:28:59 +0800

    更新

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -82,7 +82,10 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
-    blog: true,
+    // blog: true,
+    blog: {
+      excerptLength:0,
+    },
     // 评论
     comment: {
       provider: "Waline",

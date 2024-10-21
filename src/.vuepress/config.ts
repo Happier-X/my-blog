97b277e1f69514fd8e35e7768033171f34ecfe20
赵鸿飞 <zhaohongfei@vasoyn.com>
2024-10-21 17:51:00 +0800

    fix: 修改描述

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,21 +1,13 @@
 import { getDirname, path } from "vuepress/utils";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
 const __dirname = getDirname(import.meta.url);
-
 export default defineUserConfig({
   base: "/",
-
   lang: "zh-CN",
   title: "Happier's Blog",
-  description: "分享我的学习和生活，希望你能有所收获",
-
+  description: "悟已往之不谏，知来者之可追。",
   theme,
-
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
-
   alias: {
     "@theme-hope/modules/blog/components/BlogHero": path.resolve(
       __dirname,

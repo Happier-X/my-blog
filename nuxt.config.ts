bb23d370e662a000d9082dbba5508d0125e4544b
赵鸿飞 <zhf5256@qq.com>
2025-12-23 11:04:48 +0800

    feat: 引入 Nuxt Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "nuxt-studio"],
   devtools: { enabled: true },
   compatibilityDate: "2024-04-03",
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  studio: {
+    repository: {
+      provider: "github", // 'github' or 'gitlab'
+      owner: "Happier-X",
+      repo: "my-blog",
+      branch: "main",
+    },
   },
 });

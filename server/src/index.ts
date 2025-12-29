0a9b8d3d96833a0a1b585f78b32356cca0d57361
赵鸿飞 <zhf5256@qq.com>
2025-12-29 15:07:02 +0800

    feat: 引入Prisma并初始化数据库

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import type { PrismaClient } from "./generated/prisma/client.js";
+import withPrisma from "./lib/prisma.js";
 
-const app = new Hono()
+type ContextWithPrisma = {
+  Variables: {
+    prisma: PrismaClient;
+  };
+};
+
+const app = new Hono<ContextWithPrisma>();
 
 const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+  "Hello Hono!",
+  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
+];
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+app
+  .get("/", (c) => {
+    return c.text(welcomeStrings.join("\n\n"));
+  })
+  .get("/users", withPrisma, async (c) => {
+    const prisma = c.get("prisma");
+    const users = await prisma.user.findMany();
+    return c.json(users);
+  });
 
-export default app
+export default app;

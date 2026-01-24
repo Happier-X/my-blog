8db6c0e8dd29b1ef35ce9d5262ad4899344518a7
Happier <zhf5256@qq.com>
2026-01-24 18:36:57 +0800

    feat: 更新依赖

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <u-button color="primary">按钮</u-button>
-    <div class="bg-blue-500">测试</div>
+    <NuxtWelcome />
   </div>
 </template>
--- app/plugins/hakimi-ui.ts
+++
@@ -1,5 +0,0 @@
-import HakimiUi from 'hakimi-ui'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(HakimiUi)
-})
